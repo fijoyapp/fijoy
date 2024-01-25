9f658fb01174010601d9cac323841ebd3ee174bb
Joey Yu <joey@jyu.dev>
2024-01-25 15:26:45 -0500

    fix: include tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import "./index.css";
+
 // Create a new router instance
 const router = createRouter({ routeTree });
 
