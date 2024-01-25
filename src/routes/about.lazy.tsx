484498525a8a2479aee83f394f19ac92595afc57
Joey Yu <joey@jyu.dev>
2024-01-25 15:25:48 -0500

    feat: prettier

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,5 +1,5 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/about')({
-  component: () => <div>Hello /about!</div>
-})
+export const Route = createLazyFileRoute("/about")({
+  component: () => <div>Hello /about!</div>,
+});
