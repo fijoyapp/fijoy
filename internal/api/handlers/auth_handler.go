f1f52966aaf08c99fd4d38a2231a6a235d8f6664
Joey Yu <joey@jyu.dev>
2024-01-27 15:15:13 -0500

    chore: redirect to home

--- internal/api/handlers/auth_handler.go
+++ internal/api/handlers/auth_handler.go
@@ -142,7 +142,7 @@ func (ah *authHandler) googleCallback(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.SetCookie(w, cookie)
-	http.Redirect(w, r, ah.frontendUrl+"/workspace", http.StatusFound)
+	http.Redirect(w, r, ah.frontendUrl, http.StatusFound)
 }
 
 func generateStateOAuthCookie(w http.ResponseWriter) string {
