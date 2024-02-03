611c0b40cb8c6b1524bd517946317131d36aae2c
Joey Yu <joey@jyu.dev>
2024-02-02 22:40:14 -0500

    chore: add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider";
 import { AuthProvider, useAuth } from "./auth";
+import { Toaster } from "./components/ui/sonner";
 
 export const queryClient = new QueryClient();
 
@@ -46,6 +47,7 @@ if (!rootElement.innerHTML) {
         <AuthProvider>
           <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <InnerApp />
+            <Toaster />
           </ThemeProvider>
         </AuthProvider>
       </QueryClientProvider>
