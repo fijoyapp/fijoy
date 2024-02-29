bbec94b3977a722414c6856f1f366476e02dfff7
Joey Yu <joey@jyu.dev>
2024-02-28 21:49:04 -0500

    feat(kanel): integrate kanel

---
+++ apps/web/.kanelrc.cjs
@@ -0,0 +1,81 @@
+const {
+  makeGenerateZodSchemas,
+  defaultGetZodSchemaMetadata,
+  defaultGetZodIdentifierMetadata,
+  defaultZodTypeMap,
+} = require("kanel-zod");
+const { resolveType } = require("kanel");
+
+const { recase } = require("@kristiandupont/recase");
+const { tryParse } = require("tagged-comment-parser");
+
+const generateZodSchemas = makeGenerateZodSchemas({
+  getZodSchemaMetadata: defaultGetZodSchemaMetadata,
+  getZodIdentifierMetadata: defaultGetZodIdentifierMetadata,
+  zodTypeMap: {
+    ...defaultZodTypeMap,
+    "pg_catalog.timestamptz": "z.coerce.date()",
+  },
+  castToSchema: false,
+});
+
+const toCamelCase = recase("snake", "camel");
+const toPascalCase = recase("snake", "pascal");
+const outputPath = "./src/schemas";
+
+/** @type {import('kanel').Config} */
+module.exports = {
+  connection: process.env.DB_URL,
+
+  preDeleteOutputFolder: true,
+  outputPath,
+  preRenderHooks: [generateZodSchemas],
+
+  enumStyle: "type",
+
+  customTypeMap: {
+    "pg_catalog.tsvector": "string",
+    "pg_catalog.bpchar": "string",
+  },
+
+  getPropertyMetadata: (property, _details, generateFor) => {
+    const { comment: strippedComment } = tryParse(property.comment);
+
+    return {
+      name: toCamelCase(property.name),
+      comment: [
+        `Database type: ${property.expandedType}`,
+        ...(generateFor === "initializer" && property.defaultValue
+          ? [`Default value: ${property.defaultValue}`]
+          : []),
+        ...(strippedComment ? [strippedComment] : []),
+      ],
+    };
+  },
+
+  generateIdentifierType: (column, details, config) => {
+    const name = toPascalCase(details.name) + toPascalCase(column.name);
+    const innerType = resolveType(column, details, {
+      ...config,
+      // Explicitly disable identifier resolution so we get the actual inner type here
+      generateIdentifierType: undefined,
+    });
+    const imports = [];
+
+    let type = innerType;
+    if (typeof innerType === "object") {
+      // Handle non-primitives
+      type = innerType.name;
+      imports.push(...innerType.typeImports);
+    }
+
+    return {
+      declarationType: "typeDeclaration",
+      name,
+      exportAs: "named",
+      typeDefinition: [innerType],
+      typeImports: imports,
+      comment: [],
+    };
+  },
+};
