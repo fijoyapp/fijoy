d634600be2971359a4d46741b70fea9f9050aefe
Joey Yu <joey@jyu.dev>
2024-03-02 19:24:18 -0500

    chore: init workspace proto

--- apps/server/internal/database/migrations/000002_create_fijoy_workspace_table.up.sql
+++ apps/server/internal/database/migrations/000002_create_fijoy_workspace_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE fijoy_workspace (
   id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
   namespace TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
   CONSTRAINT "fijoy_workspace_namespace_unique" UNIQUE ("namespace")
 );
