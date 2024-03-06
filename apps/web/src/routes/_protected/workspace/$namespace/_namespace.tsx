48195441ef9538d164bcc3dca907488330b0836c
Joey Yu <joey@jyu.dev>
2024-03-06 10:47:41 -0500

    chore(web): banner

--- apps/web/src/routes/_protected/workspace/$namespace/_namespace.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/_namespace.tsx
@@ -1,4 +1,5 @@
 import PrivateSidebar from "@/components/private-sidebar";
+import { siteConfig } from "@/config/site";
 import { Workspace } from "@/gen/proto/fijoy/v1/workspace_pb";
 import { getAccountsQueryOptions } from "@/lib/queries/account";
 import { getWorkspaceByNamespaceQueryOptions } from "@/lib/queries/workspace";
@@ -35,6 +36,25 @@ export const Route = createFileRoute(
     <div className="flex w-screen">
       <PrivateSidebar className="m-4 w-56 flex-shrink-0 rounded-xl bg-muted" />
       <div className="h-screen min-w-[48rem] grow flex-nowrap overflow-y-scroll">
+        <div className="my-4 mr-4 overflow-hidden overflow-ellipsis whitespace-nowrap rounded-xl bg-muted p-4">
+          Fijoy is currently in development :) Join our{" "}
+          <a
+            href={siteConfig.links.discord}
+            target="_blank"
+            className="text-primary"
+          >
+            Discord
+          </a>{" "}
+          to follow along, and checkout our{" "}
+          <a
+            href={siteConfig.links.github}
+            target="_blank"
+            className="text-primary"
+          >
+            GitHub
+          </a>
+          !
+        </div>
         <Outlet />
       </div>
     </div>
