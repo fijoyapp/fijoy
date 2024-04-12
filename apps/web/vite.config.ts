416e7c34e8ffe492310f91d99f70b7414ab02638
Joey Yu <joey@jyu.dev>
2024-04-12 10:00:12 -0400

    chore: the swc version is weird

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
