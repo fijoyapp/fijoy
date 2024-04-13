ce6011c81a354347f2fe85997f1fdfcc75969578
Joey Yu <joey@jyu.dev>
2024-04-13 09:07:15 -0400

    fix(web): build error

--- apps/web/src/lib/example.ts
+++ apps/web/src/lib/example.ts
@@ -3,15 +3,15 @@ import { Workspace } from "@/gen/proto/fijoy/v1/workspace_pb";
 import { AccountService } from "@/gen/proto/fijoy/v1/account_connect";
 import { finalTransport } from "./connect";
 import { AccountType } from "@/gen/proto/fijoy/v1/account_pb";
-import { TransactionService } from "@/gen/proto/fijoy/v1/transaction_connect";
-import { Timestamp } from "@bufbuild/protobuf";
+// import { TransactionService } from "@/gen/proto/fijoy/v1/transaction_connect";
+// import { Timestamp } from "@bufbuild/protobuf";
 import { getWorkspaceHeader } from "./headers";
 
 const accountClient = createPromiseClient(AccountService, finalTransport);
-const transactionClient = createPromiseClient(
-  TransactionService,
-  finalTransport,
-);
+// const transactionClient = createPromiseClient(
+//   TransactionService,
+//   finalTransport,
+// );
 
 export async function populateExample(workspace: Workspace) {
   const headers = getWorkspaceHeader(workspace.id);
@@ -19,83 +19,59 @@ export async function populateExample(workspace: Workspace) {
     {
       name: "Cobalt",
       accountType: AccountType.DEBT,
-      currency: "CAD",
+      balance: {
+        currencyCode: "CAD",
+        nanos: 690000000,
+        units: BigInt(2333),
+      },
       institution: "Amex",
     },
     { headers },
   );
-  await transactionClient.createAdjustmentTransaction(
-    {
-      accountId: cobalt.id,
-      amount: {
-        units: -BigInt(2333),
-        nanos: -690000000,
-      },
-      datetime: Timestamp.now(),
-    },
-    { headers },
-  );
+  console.log(cobalt);
 
   const wealthsimpleCash = await accountClient.createAccount(
     {
       name: "Wealthsimple Cash",
       accountType: AccountType.CASH,
-      currency: "CAD",
+      balance: {
+        currencyCode: "CAD",
+        nanos: 690000000,
+        units: BigInt(69420),
+      },
       institution: "Wealthsimple",
     },
     { headers },
   );
-  await transactionClient.createAdjustmentTransaction(
-    {
-      accountId: wealthsimpleCash.id,
-      amount: {
-        units: BigInt(69420),
-        nanos: 690000000,
-      },
-      datetime: Timestamp.now(),
-    },
-    { headers },
-  );
+  console.log(wealthsimpleCash);
 
   const tangerineSavings = await accountClient.createAccount(
     {
       name: "Tangerine Savings",
       accountType: AccountType.CASH,
-      currency: "CAD",
+      balance: {
+        currencyCode: "CAD",
+        nanos: 990000000,
+        units: BigInt(9999),
+      },
       institution: "Tangerine",
     },
     { headers },
   );
-  await transactionClient.createAdjustmentTransaction(
-    {
-      accountId: tangerineSavings.id,
-      amount: {
-        units: BigInt(9999),
-        nanos: 990000000,
-      },
-      datetime: Timestamp.now(),
-    },
-    { headers },
-  );
+  console.log(tangerineSavings);
 
   const tangerineChequing = await accountClient.createAccount(
     {
       name: "Tangerine Chequing",
       accountType: AccountType.CASH,
-      currency: "CAD",
+      balance: {
+        currencyCode: "CAD",
+        nanos: 660000000,
+        units: BigInt(66),
+      },
       institution: "Tangerine",
     },
     { headers },
   );
-  await transactionClient.createAdjustmentTransaction(
-    {
-      accountId: tangerineChequing.id,
-      amount: {
-        units: BigInt(6666),
-        nanos: 660000000,
-      },
-      datetime: Timestamp.now(),
-    },
-    { headers },
-  );
+  console.log(tangerineChequing);
 }
