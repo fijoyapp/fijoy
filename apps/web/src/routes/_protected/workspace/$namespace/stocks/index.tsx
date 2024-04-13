9f4359e4331f4f495956cefdd4d289c60a49a69a
Joey Yu <joey@jyu.dev>
2024-04-12 22:02:23 -0400

    chore: format

--- apps/web/src/routes/_protected/workspace/$namespace/stocks/index.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/stocks/index.tsx
@@ -1,5 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_protected/workspace/$namespace/stocks/')({
-  component: () => <div>Hello /_protected/workspace/$namespace/stocks/!</div>
-})
+export const Route = createFileRoute(
+  "/_protected/workspace/$namespace/stocks/",
+)({
+  component: () => <div>Hello /_protected/workspace/$namespace/stocks/!</div>,
+});
