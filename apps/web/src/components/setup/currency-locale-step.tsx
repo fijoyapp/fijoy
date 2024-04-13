6b4f535571aef3179c685b3edef37411a6640317
Joey Yu <joey@jyu.dev>
2024-04-13 09:22:41 -0400

    fix(web): format

--- apps/web/src/components/setup/currency-locale-step.tsx
+++ apps/web/src/components/setup/currency-locale-step.tsx
@@ -126,7 +126,7 @@ const CurrencyLocaleStep = () => {
                                           "primaryCurrency",
                                           currency,
                                         );
-                                        setPopoverOpen(false)
+                                        setPopoverOpen(false);
                                       }}
                                     >
                                       <Check
