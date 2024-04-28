dbbd314a39e95711e8960935029d2e80e40de601
Joey Yu <joey@jyu.dev>
2024-04-28 14:04:06 -0400

    feat: show last updated

--- apps/web/src/components/accounts/columns.tsx
+++ apps/web/src/components/accounts/columns.tsx
@@ -4,6 +4,7 @@ import { Account, AccountType } from "@/gen/proto/fijoy/v1/account_pb";
 import { ColumnDef } from "@tanstack/react-table";
 import { BalanceCell } from "./balance-cell";
 import { AccountDetailButton } from "./account-detail-button";
+import { getPrettyTime } from "@/lib/time";
 
 export const columns: ColumnDef<Account>[] = [
   {
@@ -20,14 +21,24 @@ export const columns: ColumnDef<Account>[] = [
       return accountTypeConfigMap[row.original.accountType].name;
     },
   },
-  {
-    accessorKey: "institution",
-    header: "Institution",
-  },
+  // {
+  //   accessorKey: "institution",
+  //   header: "Institution",
+  // },
   {
     accessorKey: "balance",
     header: "Balance",
     cell: BalanceCell,
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "Last Updated",
+    cell: ({ row }) => {
+      if (!row.original.updatedAt) {
+        return "Unknown";
+      }
+      return <div>{getPrettyTime(row.original.updatedAt.toDate())}</div>;
+    },
   },
   {
     id: "actions",
