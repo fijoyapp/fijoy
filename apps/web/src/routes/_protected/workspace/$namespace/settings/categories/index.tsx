6bcc058816a685ad1a3c80606643262e089145b4
Joey Yu <joey@jyu.dev>
2024-04-30 21:30:00 -0400

    chore: some style adjustments

--- apps/web/src/routes/_protected/workspace/$namespace/settings/categories/index.tsx
+++ apps/web/src/routes/_protected/workspace/$namespace/settings/categories/index.tsx
@@ -15,6 +15,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { ArrowLeftRight, PiggyBank, Receipt } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
 
 export const Route = createFileRoute(
   "/_protected/workspace/$namespace/settings/categories/",
@@ -46,38 +47,53 @@ function Page() {
         Tweak your transaction categories
       </p>
 
+      <div className="py-4"></div>
+
       <Accordion
         type="multiple"
         defaultValue={["expense", "income", "transfer"]}
-        className="items-start lg:flex lg:space-x-8"
+        className="items-start gap-4 space-y-4 xl:flex xl:space-y-0"
       >
-        <AccordionItem value="expense" className="lg:w-64">
-          <AccordionTrigger>
-            <Receipt className="h-4 w-4 !transform-none" />
-            Expense Categories
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="income" className="lg:w-64">
-          <AccordionTrigger>
-            <PiggyBank className="h-4 w-4 !transform-none" />
-            Income Categories
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="transfer" className="lg:w-64">
-          <AccordionTrigger>
-            <ArrowLeftRight className="h-4 w-4 !transform-none" />
-            Transfer Categories
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
+        <Card>
+          <CardContent>
+            <AccordionItem value="expense" className="border-none xl:w-64">
+              <AccordionTrigger>
+                <Receipt className="h-4 w-4 !transform-none" />
+                Expense Categories
+              </AccordionTrigger>
+              <AccordionContent className="pb-0">
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <AccordionItem value="income" className="border-none xl:w-64">
+              <AccordionTrigger>
+                <PiggyBank className="h-4 w-4 !transform-none" />
+                Income Categories
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardContent>
+            <AccordionItem value="transfer" className="border-none xl:w-64">
+              <AccordionTrigger>
+                <ArrowLeftRight className="h-4 w-4 !transform-none" />
+                Transfer Categories
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </CardContent>
+        </Card>
       </Accordion>
     </div>
   );
