82db5c705444e1fc294ff09ab31865e1fcd6de83
Joey Yu <joey@jyu.dev>
2024-08-12 20:27:10 -0400

    feat: add account button

--- apps/web/src/types/transaction.ts
+++
@@ -1,4 +0,0 @@
-import { TypeOf, z } from "zod";
-
-export const TransactionTypeEnum = z.enum(["expense", "income", "transfer"]);
-export type TransactionTypeEnum = TypeOf<typeof TransactionTypeEnum>;
