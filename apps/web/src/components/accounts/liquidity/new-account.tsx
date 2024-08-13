07896941ade7d3ce6dd81fa2ceb68d3f7e0db8ed
Joey Yu <joey@jyu.dev>
2024-08-12 21:19:07 -0400

    chore: small placeholder

--- apps/web/src/components/accounts/liquidity/new-account.tsx
+++ apps/web/src/components/accounts/liquidity/new-account.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  // CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function NewAccount() {
   return (
-    <div>
-      <div>New Liquidity</div>
+    <div className="flex flex-col space-y-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>General Information</CardTitle>
+          {/* <CardDescription> */}
+          {/*   Note that the currency cannot be changed after account creation */}
+          {/* </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Balance</CardTitle>
+          {/* <CardDescription>The initial balance of the account</CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+      </Card>
+
+      <Button className="ml-auto">Create</Button>
     </div>
   );
 }
