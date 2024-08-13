96a5b4885ba0fa19f8b840c8442fb337f6e4dcca
Joey Yu <joey@jyu.dev>
2024-08-12 20:46:54 -0400

    chore: format

--- apps/web/src/routes/_protected/_profile/home/index.tsx
+++ apps/web/src/routes/_protected/_profile/home/index.tsx
@@ -3,4 +3,3 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/_protected/_profile/home/")({
   component: () => <div>Hello /_protected/_profile/home/!</div>,
 });
-
