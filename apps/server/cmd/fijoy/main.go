1148d962edc2040810aabd390d9cbc216351db31
Joey Yu <joey@jyu.dev>
2024-08-27 14:56:25 -0400

    chore: bring back graceful shutdown

--- apps/server/cmd/fijoy/main.go
+++ apps/server/cmd/fijoy/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fijoy/config"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	auth_handler "fijoy/internal/domain/auth/handler"
 	auth_usecase "fijoy/internal/domain/auth/usecase"
@@ -100,17 +106,42 @@ func main() {
 	// Start our server
 	server := newServer(":"+cfg.Server.PORT, r)
 
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		panic(err)
+	// Server run context
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	go func() {
+		<-sig
+
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	// Run the server
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
 
-	// go func() {
-	// 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-	// 		panic(err)
-	// 	}
-	// }()
-	//
-	// waitForShutdown(server)
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
 }
 
 func newServer(addr string, r *chi.Mux) *http.Server {
