59e3f242369e11bfa952a3fbda90ea0dfd5e9bb6
Joey Yu <joey@jyu.dev>
2024-09-04 23:11:39 -0400

    chore: improve accounts list

--- apps/web/src/components/accounts/account-list.tsx
+++ apps/web/src/components/accounts/account-list.tsx
@@ -1,38 +1,80 @@
-import { Accounts, AccountType } from "@/gen/proto/fijoy/v1/account_pb";
+import { Account, Accounts } from "@/gen/proto/fijoy/v1/account_pb";
 import { LiquidityCard } from "./liquidity/card";
 import { Card } from "../ui/card";
 import { Separator } from "../ui/separator";
+import { useMemo } from "react";
+import { accountsGroupBy } from "@/lib/account";
+import { LiabilityCard } from "./liability/card";
 
 type AccountListProps = {
   accounts: Accounts;
 };
 
+type Section = {
+  name: string;
+  accounts: Account[];
+  card: React.FC<{ account: Account }>;
+};
+
 export default function AccountList({ accounts }: AccountListProps) {
-  const liquidities = accounts.accounts.filter((account) => {
-    return account.accountType === AccountType.LIQUIDITY;
-  });
+  const { liabilities, liquidities, investments, properties, receivables } =
+    useMemo(() => accountsGroupBy(accounts), [accounts]);
+
+  const sections: Section[] = [
+    {
+      name: "Liquidity",
+      accounts: liquidities,
+      card: LiquidityCard,
+    },
+    {
+      name: "Investment",
+      accounts: investments,
+      card: LiquidityCard,
+    },
+    {
+      name: "Property",
+      accounts: properties,
+      card: LiquidityCard,
+    },
+    {
+      name: "Receivable",
+      accounts: receivables,
+      card: LiquidityCard,
+    },
+    {
+      name: "Liability",
+      accounts: liabilities,
+      card: LiabilityCard,
+    },
+  ];
 
   return (
-    <div>
-      <div>
-        <div className="font-semibold">Liquidities</div>
-        <div className="py-1"></div>
+    <div className="flex-col space-y-4">
+      {sections
+        .filter((section) => section.accounts.length > 0)
+        .map((section, idx) => {
+          return (
+            <div key={idx}>
+              <div className="font-semibold">{section.name}</div>
+              <div className="py-1"></div>
 
-        <Card className="">
-          {liquidities.map((account, idx) => {
-            if (idx === 0) {
-              return <LiquidityCard key={account.id} account={account} />;
-            }
+              <Card className="">
+                {section.accounts.map((account, idx) => {
+                  if (idx === 0) {
+                    return <section.card key={account.id} account={account} />;
+                  }
 
-            return (
-              <>
-                <Separator className="" />
-                <LiquidityCard key={account.id} account={account} />
-              </>
-            );
-          })}
-        </Card>
-      </div>
+                  return (
+                    <>
+                      <Separator className="" />
+                      <section.card key={account.id} account={account} />
+                    </>
+                  );
+                })}
+              </Card>
+            </div>
+          );
+        })}
     </div>
   );
 }
