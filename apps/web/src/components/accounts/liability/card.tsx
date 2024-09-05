d8e1f5fc9b981e98ce9a8aa4481829f2076542e9
Joey Yu <joey@jyu.dev>
2024-09-04 22:56:34 -0400

    feat(account): liability create

---
+++ apps/web/src/components/accounts/liability/card.tsx
@@ -0,0 +1,44 @@
+import { CardContent } from "@/components/ui/card";
+import { Account } from "@/gen/proto/fijoy/v1/account_pb";
+import { useProfile } from "@/hooks/use-profile";
+import { getCurrencyDisplay } from "@/lib/money";
+import { getPrettyTime } from "@/lib/time";
+import { CreditCard } from "lucide-react";
+
+type LiabilityCardProps = {
+  account: Account;
+};
+
+export default function LiabilityCard({ account }: LiabilityCardProps) {
+  const { profile } = useProfile();
+  if (!profile) {
+    return null;
+  }
+
+  return (
+    <CardContent className="flex items-center space-x-2.5 p-2 px-4">
+      <div>
+        <CreditCard />
+      </div>
+
+      <div>
+        <div>{account.name}</div>
+        <div className="text-xs text-muted-foreground">{account.symbol}</div>
+      </div>
+
+      <div className="grow"></div>
+
+      <div className="flex flex-col items-end">
+        <div>
+          {getCurrencyDisplay(account.balance, account.symbol, profile.locale, {
+            compact: false,
+            isDebt: true,
+          })}
+        </div>
+        <div className="text-xs text-muted-foreground">
+          {getPrettyTime(account.updatedAt!.toDate())}
+        </div>
+      </div>
+    </CardContent>
+  );
+}
--- apps/web/src/components/accounts/liability/new-account.tsx
+++ apps/web/src/components/accounts/liability/new-account.tsx
@@ -1,7 +1,131 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  // CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { NameField } from "../form/name";
+import { CurrencyField } from "../form/currency";
+import { useProfile } from "@/hooks/use-profile";
+import { MoneyField } from "../form/money";
+import { createConnectQueryKey, useMutation } from "@connectrpc/connect-query";
+import {
+  createAccount,
+  getAccounts,
+} from "@/gen/proto/fijoy/v1/account-AccountService_connectquery";
+import {
+  AccountSymbolType,
+  AccountType,
+} from "@/gen/proto/fijoy/v1/account_pb";
+import { useRouter } from "@tanstack/react-router";
+import { toast } from "sonner";
+import { getProfileHeader } from "@/lib/headers";
+import { useQueryClient } from "@tanstack/react-query";
+import currency from "currency.js";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  symbol: z.string().length(3),
+  balance: z.string(),
+});
+
 export default function NewAccount() {
+  const { profile } = useProfile();
+
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      symbol: profile?.currencies[0],
+    },
+  });
+
+  const createAccountMut = useMutation(createAccount, {
+    callOptions: {
+      headers: getProfileHeader(profile?.id ?? ""),
+    },
+    onSuccess: async () => {
+      queryClient.invalidateQueries({
+        queryKey: createConnectQueryKey(getAccounts),
+      });
+      router.navigate({
+        to: "/accounts",
+      });
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast.promise(
+      createAccountMut.mutateAsync({
+        name: values.name,
+        accountType: AccountType.LIQUIDITY,
+
+        includeInNetWorth: true,
+        symbol: values.symbol,
+        symbolType: AccountSymbolType.CURRENCY,
+
+        amount: currency(values.balance).multiply(-1).toString(),
+        value: "1",
+        fxRate: "1",
+      }),
+      {
+        success: () => {
+          return "Account created";
+        },
+        loading: "Creating account...",
+        error: (e: Error) => `Error creating account: ${e.toString()}`,
+      },
+    );
+  }
+
   return (
-    <div>
-      <div>New Liquidity</div>
+    <div className="flex max-w-lg flex-col space-y-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <Card>
+            <CardHeader>
+              <CardTitle>New Liability Account</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <NameField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Give your account a descriptive name, e.g. My Student Loan"
+              />
+
+              <CurrencyField
+                control={form.control}
+                name="symbol"
+                label="Account Currency"
+                description="Note that the currency cannot be changed after account creation"
+              />
+
+              <MoneyField
+                control={form.control}
+                name="balance"
+                label="Balance"
+                currency={form.getValues("symbol")}
+                onValueChange={(value) => form.setValue("balance", value)}
+                description="Positive balance represents debt, negative balance represents asset"
+              />
+            </CardContent>
+          </Card>
+
+          <Button className="ml-auto">Create</Button>
+        </form>
+      </Form>
     </div>
   );
 }
