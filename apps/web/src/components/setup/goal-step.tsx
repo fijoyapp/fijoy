457d7a6b5bda070635b8df358b8de232c614c8de
Joey Yu <joey@jyu.dev>
2024-09-09 22:10:01 -0400

    feat: setup net worth goal on signup

---
+++ apps/web/src/components/setup/goal-step.tsx
@@ -0,0 +1,73 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { Navigate, useRouter } from "@tanstack/react-router";
+import { GoalStepData } from "@/types/setup";
+import { useSetupStore } from "@/store/setup";
+import { type TypeOf } from "zod";
+import { MoneyField } from "./form/money";
+import { Currencies } from "@/gen/proto/fijoy/v1/currency_pb";
+
+const formSchema = GoalStepData;
+
+type GoalStepProps = {
+  currencies: Currencies;
+};
+
+const GoalStep = ({ currencies }: GoalStepProps) => {
+  const router = useRouter();
+
+  const { currencyStepData, goalStepData, setGoalStepData } = useSetupStore(
+    (state) => ({
+      goalStepData: state.goalStepData,
+      setGoalStepData: state.setGoalStepData,
+      currencyStepData: state.currencyStepData,
+    }),
+  );
+
+  const form = useForm<TypeOf<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: goalStepData,
+  });
+
+  function onSubmit(values: TypeOf<typeof formSchema>) {
+    setGoalStepData(values);
+    router.navigate({
+      from: "/setup",
+      to: "/setup",
+      search: { step: "final" },
+    });
+  }
+
+  if (!currencyStepData) {
+    return <Navigate to="/setup" search={{ step: "currency" }} />;
+  }
+
+  const locale = currencies.currencies.find(
+    (currency) => currency.code === currencyStepData.currencies[0],
+  )!.locale;
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <MoneyField
+            control={form.control}
+            name="net_worth_goal"
+            label="Net Worth Goal"
+            currency={currencyStepData.currencies[0]}
+            locale={locale}
+            onValueChange={(value) => form.setValue("net_worth_goal", value)}
+          />
+
+          <Button type="submit" className="col-span-2">
+            Create
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default GoalStep;
--- apps/web/src/store/setup.ts
+++ apps/web/src/store/setup.ts
@@ -1,29 +1,39 @@
-import { CurrencyStepData } from "@/types/setup";
+import { CurrencyStepData, GoalStepData } from "@/types/setup";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 type State = {
   currencyStepData: CurrencyStepData | undefined;
+
+  goalStepData: GoalStepData | undefined;
 };
 
 type Actions = {
   setCurrencyStepData: (step: CurrencyStepData) => void;
+  setGoalStepData: (step: GoalStepData) => void;
   reset: () => void;
 };
 
 export const useSetupStore = create<State & Actions>()(
   immer((set) => ({
     currencyStepData: undefined,
+    goalStepData: undefined,
 
     setCurrencyStepData: (step) => {
       set((state) => {
         state.currencyStepData = step;
       });
     },
+    setGoalStepData: (step) => {
+      set((state) => {
+        state.goalStepData = step;
+      });
+    },
 
     reset: () =>
       set((state) => {
         state.currencyStepData = undefined;
+        state.goalStepData = undefined;
       }),
   })),
 );
