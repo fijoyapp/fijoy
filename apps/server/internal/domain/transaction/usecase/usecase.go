2695f5a37fd99b6f7b9ddf41e94ec13d58a0ce20
Joey Yu <joey@jyu.dev>
2024-10-13 19:36:05 -0400

    chore: group snapshot

--- apps/server/internal/domain/transaction/usecase/usecase.go
+++ apps/server/internal/domain/transaction/usecase/usecase.go
@@ -5,12 +5,15 @@ import (
 	"errors"
 	"fijoy/constants"
 	"fijoy/ent"
+	"fijoy/ent/account"
 	account_repository "fijoy/internal/domain/account/repository"
 	snapshot_repository "fijoy/internal/domain/snapshot/repository"
 	transaction_repository "fijoy/internal/domain/transaction/repository"
+	"fijoy/internal/util/database"
 	fijoyv1 "fijoy/proto/fijoy/v1"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/shopspring/decimal"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -78,8 +81,82 @@ func transactionsModelToProto(transactions []*ent.Transaction) *fijoyv1.Transact
 }
 
 func (u *transactionUseCase) CreateTransaction(ctx context.Context, profileId string, req *fijoyv1.CreateTransactionRequest) (*fijoyv1.Transaction, error) {
-	// TODO: Implement this method
-	return nil, nil
+	var transaction *ent.Transaction
+
+	err := database.WithTx(ctx, u.client, func(tx *ent.Tx) error {
+		var err error
+
+		targetAccount, err := u.accountRepo.GetAccount(ctx, tx.Client(), req.AccountId)
+		if err != nil {
+			return err
+		}
+
+		id, err := targetAccount.QueryProfile().OnlyID(ctx)
+		if err != nil {
+			return err
+		}
+		if id != profileId {
+			return errors.New(constants.ErrAccountNotFound)
+		}
+
+		transaction, err = u.transactionRepo.CreateTransaction(ctx, tx.Client(), transaction_repository.CreateTransactionRequest{
+			OldAmount:   targetAccount.Amount,
+			AmountDelta: decimal.RequireFromString(req.AmountDelta),
+			Value:       decimal.RequireFromString(req.Value),
+			FxRate:      decimal.RequireFromString(req.FxRate),
+			OldBalance:  targetAccount.Balance,
+			Note:        req.Note,
+		})
+		if err != nil {
+			return nil
+		}
+
+		_, err = u.snapshotRepo.CreateAccountSnapshot(ctx, tx.Client(), snapshot_repository.CreateAccountSnapshotRequest{
+			Datehour: transaction.CreatedAt,
+			Balance:  transaction.Balance,
+		})
+		if err != nil {
+			return nil
+		}
+
+		lastestOverallSnapshot, err := u.snapshotRepo.GetLatestOverallSnapshot(ctx, tx.Client(), profileId)
+		if err != nil {
+			return err
+		}
+
+		switch targetAccount.AccountType {
+		case account.AccountTypeLiquidity:
+			lastestOverallSnapshot.Liquidity = lastestOverallSnapshot.Liquidity.Add(transaction.BalanceDelta)
+		case account.AccountTypeInvestment:
+			lastestOverallSnapshot.Investment = lastestOverallSnapshot.Investment.Add(transaction.BalanceDelta)
+		case account.AccountTypeProperty:
+			lastestOverallSnapshot.Property = lastestOverallSnapshot.Property.Add(transaction.BalanceDelta)
+		case account.AccountTypeReceivable:
+			lastestOverallSnapshot.Receivable = lastestOverallSnapshot.Receivable.Add(transaction.BalanceDelta)
+		case account.AccountTypeLiability:
+			lastestOverallSnapshot.Liability = lastestOverallSnapshot.Liability.Add(transaction.BalanceDelta)
+		}
+
+		_, err = u.snapshotRepo.CreateOverallSnapshot(
+			ctx, tx.Client(), snapshot_repository.CreateOverallSnapshotRequest{
+				Datehour:   transaction.CreatedAt,
+				Liquidity:  lastestOverallSnapshot.Liquidity,
+				Investment: lastestOverallSnapshot.Investment,
+				Property:   lastestOverallSnapshot.Property,
+				Receivable: lastestOverallSnapshot.Receivable,
+				Liability:  lastestOverallSnapshot.Liability,
+			})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return transactionModelToProto(transaction), nil
 }
 
 func (u *transactionUseCase) GetTransaction(ctx context.Context, profileId string, req *fijoyv1.GetTransactionRequest) (*fijoyv1.Transaction, error) {
