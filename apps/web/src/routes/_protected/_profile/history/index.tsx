f32054612a8b29744b947bfb847d87970c4df809
Joey Yu <joey@jyu.dev>
2024-10-12 22:10:32 -0400

    chore: clean proto

--- apps/web/src/routes/_protected/_profile/history/index.tsx
+++ apps/web/src/routes/_protected/_profile/history/index.tsx
@@ -55,7 +55,6 @@ function Page() {
       {/* <NewTransaction */}
       {/*   accounts={accounts} */}
       {/*   workspace={context.workspace} */}
-      {/*   categories={[]} // FIXME: load categories */}
       {/* /> */}
     </div>
   );
