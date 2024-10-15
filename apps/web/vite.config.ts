05c5462da762be90862a3d1ef43d4ff2e60d03b4
Joey Yu <joey@jyu.dev>
2024-10-14 23:30:18 -0400

    fix: format

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,18 +6,20 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), sentryVitePlugin({
-    org: "fijoy",
-    project: "web"
-  })],
-
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    sentryVitePlugin({
+      org: "fijoy",
+      project: "web",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
