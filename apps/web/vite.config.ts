0543b15e5af0c2f138fd2967a638b6999a35b893
Joey Yu <joey@jyu.dev>
2024-10-14 23:34:43 -0400

    chore: set release version

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
     sentryVitePlugin({
       org: "fijoy",
       project: "web",
+      release: {
+        name: packageJson.version,
+      },
     }),
   ],
   resolve: {
