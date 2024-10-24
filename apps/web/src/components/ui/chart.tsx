d568a197720b409241238de1d6062ffb6efc02e7
Joey Yu <joey@jyu.dev>
2024-10-23 22:57:47 -0400

    fix: eslint

--- apps/web/src/components/ui/chart.tsx
+++ apps/web/src/components/ui/chart.tsx
@@ -67,6 +67,7 @@ ChartContainer.displayName = "Chart";
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     ([_, config]) => config.theme || config.color,
   );
 
