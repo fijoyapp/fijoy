c21219c5fbcd6eb5176f3ad6525e3de5d648389b
Joey Yu <joey@jyu.dev>
2024-12-29 00:37:45 -0500

    fix: format

--- apps/web/src/routes/_protected/_profile/settings/general/index.tsx
+++ apps/web/src/routes/_protected/_profile/settings/general/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_protected/_profile/settings/general/')({
+export const Route = createFileRoute("/_protected/_profile/settings/general/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_protected/_profile/settings/general/"!</div>
+  return <div>Hello "/_protected/_profile/settings/general/"!</div>;
 }
