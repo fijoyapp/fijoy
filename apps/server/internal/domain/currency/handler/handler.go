d0532f272d488976583ce127f13440449822fd82
Joey Yu <joey@jyu.dev>
2025-02-03 18:25:48 -0500

    chore: use lo

--- apps/server/internal/domain/currency/handler/handler.go
+++ apps/server/internal/domain/currency/handler/handler.go
@@ -6,6 +6,7 @@ import (
 	fijoyv1 "fijoy/proto/fijoy/v1"
 
 	"connectrpc.com/connect"
+	"github.com/samber/lo"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -19,13 +20,12 @@ func (h *currencyHandler) GetCurrencies(
 	ctx context.Context,
 	req *connect.Request[emptypb.Empty],
 ) (*connect.Response[fijoyv1.CurrencyList], error) {
-	currencies := []*fijoyv1.Currency{}
-
-	for _, currency := range constants.Currencies {
-		currencies = append(currencies, &fijoyv1.Currency{
-			Code: currency.Code, Locale: currency.Locale,
+	currencies := lo.Map(lo.Values(constants.Currencies),
+		func(currency constants.Currency, _ int) *fijoyv1.Currency {
+			return &fijoyv1.Currency{
+				Code: currency.Code, Locale: currency.Locale,
+			}
 		})
-	}
 
 	return connect.NewResponse(&fijoyv1.CurrencyList{
 		Items: currencies,
