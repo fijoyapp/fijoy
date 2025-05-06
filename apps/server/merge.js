e3f56c89d8db68c8648ce87b3791a983b8b7359e
Joey Yu <joey@jyu.dev>
2025-05-06 15:56:13 -0400

    fix: format graphql

--- apps/server/merge.js
+++ apps/server/merge.js
@@ -6,10 +6,14 @@ import { print } from "graphql";
 import { writeFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import * as prettier from "prettier";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 const loadedFiles = loadFilesSync(`${__dirname}/*.graphql`);
 const typeDefs = mergeTypeDefs(loadedFiles);
 const printedTypeDefs = print(typeDefs);
-writeFileSync("relay.graphql", printedTypeDefs);
+const formattedTypeDefs = await prettier.format(printedTypeDefs, {
+  parser: "graphql",
+});
+writeFileSync("relay.graphql", formattedTypeDefs);
