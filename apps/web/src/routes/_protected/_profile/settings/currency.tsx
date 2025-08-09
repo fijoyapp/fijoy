7eacd00778cec2028347c1c27035386f7f52c0c6
Joey Yu <joey@jyu.dev>
2025-08-09 15:17:29 -0400

    style: align currency style

--- apps/web/src/routes/_protected/_profile/settings/currency.tsx
+++ apps/web/src/routes/_protected/_profile/settings/currency.tsx
@@ -1,14 +1,6 @@
-import { Link, createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { z, type TypeOf } from "zod";
 import { useForm } from "react-hook-form";
@@ -77,85 +69,60 @@ function Page() {
   }
 
   return (
-    <div className="max-w-xl p-4 lg:p-6">
-      <Breadcrumb>
-        <BreadcrumbList className="text-muted-foreground text-lg font-semibold md:text-2xl">
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link from={Route.fullPath} to={".."}>
-                Settings
-              </Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage className="text-foreground font-semibold">
-              Currency
-            </BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <p className="text-muted-foreground text-sm">
-        Everything related to multi-currency settings.
-      </p>
+    <div className="max-w-xl">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onUpdateCurrencySubmit)}
+          className="space-y-8"
+        >
+          <Card>
+            <div className="py-3"></div>
 
-      <div className="py-2"></div>
-
-      <div className="space-y-4">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onUpdateCurrencySubmit)}
-            className="space-y-8"
-          >
-            <Card>
-              <div className="py-3"></div>
-
-              <CardContent>
-                <CurrencyField
-                  control={form.control}
-                  name="currencies"
-                  currencies={data.currencies}
-                  defaultValues={[...profile.currencies]}
-                  onValueChange={(value) => {
-                    form.setValue("currencies", value);
-                  }}
-                />
-              </CardContent>
-              <CardFooter className="space-x-4 border-t px-6 py-4">
-                <AnimatePresence mode="wait" initial={false}>
-                  <Button
-                    disabled={form.formState.isSubmitting || isMutationInFlight}
-                    className="w-16"
-                  >
-                    {form.formState.isSubmitting || isMutationInFlight ? (
-                      <motion.span
-                        key="checkmark"
-                        variants={variants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                      >
-                        <Icons.spinner />
-                      </motion.span>
-                    ) : (
-                      <motion.span
-                        key="copy"
-                        variants={variants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                      >
-                        Save
-                      </motion.span>
-                    )}
-                  </Button>
-                </AnimatePresence>
-                <FormMessage />
-              </CardFooter>
-            </Card>
-          </form>
-        </Form>
-      </div>
+            <CardContent>
+              <CurrencyField
+                control={form.control}
+                name="currencies"
+                currencies={data.currencies}
+                defaultValues={[...profile.currencies]}
+                onValueChange={(value) => {
+                  form.setValue("currencies", value);
+                }}
+              />
+            </CardContent>
+            <CardFooter className="space-x-4 border-t px-6 py-4">
+              <AnimatePresence mode="wait" initial={false}>
+                <Button
+                  disabled={form.formState.isSubmitting || isMutationInFlight}
+                  className="w-16"
+                >
+                  {form.formState.isSubmitting || isMutationInFlight ? (
+                    <motion.span
+                      key="checkmark"
+                      variants={variants}
+                      initial="hidden"
+                      animate="visible"
+                      exit="hidden"
+                    >
+                      <Icons.spinner />
+                    </motion.span>
+                  ) : (
+                    <motion.span
+                      key="copy"
+                      variants={variants}
+                      initial="hidden"
+                      animate="visible"
+                      exit="hidden"
+                    >
+                      Save
+                    </motion.span>
+                  )}
+                </Button>
+              </AnimatePresence>
+              <FormMessage />
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </div>
   );
 }
