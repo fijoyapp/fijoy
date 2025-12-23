d4a2a7656ee5afc48be08e28db9b0c2168e774e2
Joey Yu <joey@jyu.dev>
2025-12-23 13:48:27 -0500

    rename

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -15,7 +15,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'fijoy.app',
       },
     ],
     links: [
