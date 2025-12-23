70c42a33be80339dfdf2af67dbbc320f902d5a5c
Joey Yu <joey@jyu.dev>
2025-12-23 14:25:04 -0500

    remove height

--- web/src/routes/_user/household/index.tsx
+++ web/src/routes/_user/household/index.tsx
@@ -28,10 +28,7 @@ function RouteComponent() {
         <header className="flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1 " />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-3"
-            />
+            <Separator orientation="vertical" className="mr-2" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
