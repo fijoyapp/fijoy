8e6f95fe62a40ca65a8dd615a9b553d1b6a60bd9
Joey Yu <joey@jyu.dev>
2025-12-28 15:19:15 -0500

    delete starter

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,8 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { ComponentExample } from '@/components/component-example'
+import { createFileRoute, Navigate } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <ComponentExample />
+  return <Navigate to="/login" />
 }
