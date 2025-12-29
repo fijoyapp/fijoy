7cf70e64e102f6c8ed8cc434586406d97995e489
Joey Yu <joey@jyu.dev>
2025-12-29 10:23:57 -0500

    theme

--- web/src/routes/_user/household/$householdId/route.tsx
+++ web/src/routes/_user/household/$householdId/route.tsx
@@ -65,13 +65,15 @@ function RouteComponent() {
             </div>
             <div className="grow"></div>
             <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Button variant="outline">
-                  <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-              </DropdownMenuTrigger>
+              <DropdownMenuTrigger
+                render={
+                  <Button variant="outline">
+                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+                }
+              ></DropdownMenuTrigger>
               <DropdownMenuContent align="end">
                 <DropdownMenuItem onClick={() => setTheme('light')}>
                   Light
