0328badab66e1f0282f6b457f4d6423323bbea2a
Joey Yu <joey@jyu.dev>
2025-12-30 22:11:43 -0500

    fix

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -13,7 +13,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     rules: {
