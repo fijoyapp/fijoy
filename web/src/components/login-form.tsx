0c61164eb17818f2b3c21b1dc51d3a86d2b67e8e
Joey Yu <joey@jyu.dev>
2026-01-07 20:15:27 -0500

    add logo

--- web/src/components/login-form.tsx
+++ web/src/components/login-form.tsx
@@ -27,9 +27,7 @@ export function LoginForm({
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">
-            Fijoy - Joyful net worth tracking
-          </CardTitle>
+          <CardTitle className="text-xl">Joyful net worth tracking</CardTitle>
           <CardDescription>Login with your Google account</CardDescription>
         </CardHeader>
         <CardContent>
--- web/src/routes/login.index.tsx
+++ web/src/routes/login.index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { GalleryVerticalEnd } from 'lucide-react'
 import { LoginForm } from '@/components/login-form'
+import { FijoyLogo } from '@/components/fijoy-logo'
 
 export const Route = createFileRoute('/login/')({
   component: RouteComponent,
@@ -10,12 +10,12 @@ function RouteComponent() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        {/* <a href="#" className="flex items-center gap-2 self-center font-medium"> */}
-        {/*   <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"> */}
-        {/*     <GalleryVerticalEnd className="size-4" /> */}
-        {/*   </div> */}
-        {/*   Acme Inc. */}
-        {/* </a> */}
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <FijoyLogo size={24} className="rounded-xl" />
+          </div>
+          Fijoy
+        </a>
         <LoginForm />
       </div>
     </div>
