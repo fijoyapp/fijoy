a21784180ae7f8bb494a1103dd9f3d5ed81b31d2
Joey Yu <joey@jyu.dev>
2026-01-07 20:16:13 -0500

    use link

--- web/src/routes/login.index.tsx
+++ web/src/routes/login.index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { LoginForm } from '@/components/login-form'
 import { FijoyLogo } from '@/components/fijoy-logo'
 
@@ -10,12 +10,15 @@ function RouteComponent() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <Link
+          to="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <FijoyLogo size={24} className="rounded-xl" />
           </div>
           Fijoy
-        </a>
+        </Link>
         <LoginForm />
       </div>
     </div>
