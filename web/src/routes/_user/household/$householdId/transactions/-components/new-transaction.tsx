a74e56850fd8568c4014653bcf9d8352431c7788
Joey Yu <joey@jyu.dev>
2026-01-14 18:06:37 -0500

    persist

--- web/src/routes/_user/household/$householdId/transactions/-components/new-transaction.tsx
+++ web/src/routes/_user/household/$householdId/transactions/-components/new-transaction.tsx
@@ -30,6 +30,7 @@ export function NewTransaction({ fragmentRef }: NewTransactionProps) {
 
   const handleClose = () => {
     navigate({
+      to: '.',
       search: (prev) => ({ ...prev, showNewTransaction: false }),
     })
   }
