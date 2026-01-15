c4ce1089e78d15677a4f37913e153e28692348e5
Joey Yu <joey@jyu.dev>
2026-01-14 21:24:17 -0500

    fix missing filter

--- web/src/routes/_user/household/$householdId/categories/$categoryId.tsx
+++ web/src/routes/_user/household/$householdId/categories/$categoryId.tsx
@@ -5,18 +5,22 @@ import type { CategoryIdQuery } from './__generated__/CategoryIdQuery.graphql'
 import { environment } from '@/environment'
 import { PendingComponent } from '@/components/pending-component'
 import { TransactionsList } from '../transactions/-components/transactions-list'
+import { parseDateRangeFromURL } from '@/lib/date-range'
 
 export const Route = createFileRoute(
   '/_user/household/$householdId/categories/$categoryId',
 )({
   component: RouteComponent,
-  loader: ({ params }) => {
+  beforeLoad: ({ params, search }) => {
+    const period = parseDateRangeFromURL(search.start, search.end)
     return loadQuery<CategoryIdQuery>(
       environment,
       CategoryIdQuery,
       {
         where: {
           hasCategoryWith: [{ id: params.categoryId }],
+          datetimeGTE: period.startDate,
+          datetimeLT: period.endDate,
         },
       },
       { fetchPolicy: 'store-or-network' },
@@ -32,7 +36,7 @@ const CategoryIdQuery = graphql`
 `
 
 function RouteComponent() {
-  const queryRef = Route.useLoaderData()
+  const queryRef = Route.useRouteContext()
 
   const data = usePreloadedQuery<CategoryIdQuery>(CategoryIdQuery, queryRef)
 
