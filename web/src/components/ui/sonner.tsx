7294cbbf06fe92800598655bc5fa299554c9cfbf
Joey Yu <joey@jyu.dev>
2026-01-15 21:41:52 -0500

    no next theme

--- web/src/components/ui/sonner.tsx
+++ web/src/components/ui/sonner.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from 'next-themes'
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
 import { HugeiconsIcon } from '@hugeicons/react'
 import {
@@ -8,6 +7,7 @@ import {
   MultiplicationSignCircleIcon,
   Loading03Icon,
 } from '@hugeicons/core-free-icons'
+import { useTheme } from '../theme-provider'
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = 'system' } = useTheme()
