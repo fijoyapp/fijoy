3d53eba02cc87c92103b431a2c01163df85f55b1
Rohan Hasabe <rohanhasabe8@gmail.com>
2026-01-18 18:32:26 -0500

    add investments page

--- web/src/routes/_user/household/$householdId/investments/$investmentId.tsx
+++ web/src/routes/_user/household/$householdId/investments/$investmentId.tsx
@@ -1,13 +1,66 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  loadQuery,
+  usePreloadedQuery,
+  useSubscribeToInvalidationState,
+} from 'react-relay'
+import { graphql, ROOT_ID } from 'relay-runtime'
+import { TransactionsList } from '../transactions/-components/transactions-list'
+import type { InvestmentIdQuery } from './__generated__/InvestmentIdQuery.graphql'
+import { environment } from '@/environment'
+import { PendingComponent } from '@/components/pending-component'
 
 export const Route = createFileRoute(
   '/_user/household/$householdId/investments/$investmentId',
 )({
   component: RouteComponent,
+  loader: ({ params }) => {
+    return loadQuery<InvestmentIdQuery>(
+      environment,
+      InvestmentIdQuery,
+      {
+        where: {
+          hasInvestmentLotsWith: [
+            { hasInvestmentWith: [{ id: params.investmentId }] },
+          ],
+        },
+      },
+      { fetchPolicy: 'store-or-network' },
+    )
+  },
+  pendingComponent: PendingComponent,
 })
 
+const InvestmentIdQuery = graphql`
+  query InvestmentIdQuery($where: TransactionWhereInput) {
+    ...transactionsListFragment @arguments(where: $where)
+  }
+`
+
 function RouteComponent() {
+  const params = Route.useParams()
+  const queryRef = Route.useLoaderData()
+
+  const data = usePreloadedQuery<InvestmentIdQuery>(InvestmentIdQuery, queryRef)
+
+  useSubscribeToInvalidationState([ROOT_ID], () => {
+    return loadQuery<InvestmentIdQuery>(
+      environment,
+      InvestmentIdQuery,
+      {
+        where: {
+          hasInvestmentLotsWith: [
+            { hasInvestmentWith: [{ id: params.investmentId }] },
+          ],
+        },
+      },
+      { fetchPolicy: 'network-only' },
+    )
+  })
+
   return (
-    <div>Hello "/_user/household/$householdId/investments/$investmentId"!</div>
+    <div>
+      <TransactionsList fragmentRef={data} />
+    </div>
   )
 }
