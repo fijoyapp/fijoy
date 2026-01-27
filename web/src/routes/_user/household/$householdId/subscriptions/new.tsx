c5a661fe7b71282d852b17aa3fbe8a993e253c78
Joey Yu <joey@jyu.dev>
2026-01-27 13:19:57 -0500

    add new route
    Signed-off-by: Joey Yu <joey@jyu.dev>

---
+++ web/src/routes/_user/household/$householdId/subscriptions/new.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute(
+  '/_user/household/$householdId/subscriptions/new',
+)({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/_user/household/$householdId/subscriptions/new"!</div>
+}
