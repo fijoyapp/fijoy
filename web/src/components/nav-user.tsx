3e8d92c652d104b2dd924db1c39e4d3c6dbf8767
Joey Yu <joey@jyu.dev>
2026-01-31 20:29:26 -0500

    clean up
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- web/src/components/nav-user.tsx
+++ web/src/components/nav-user.tsx
@@ -49,8 +49,10 @@ export function NavUser({
                 className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">JY</AvatarFallback>
+                  <AvatarImage src={user.avatar} alt={user.name.slice(0, 2)} />
+                  <AvatarFallback className="rounded-lg">
+                    {user.name.slice(0, 2)}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
@@ -70,8 +72,13 @@ export function NavUser({
               <DropdownMenuLabel className="p-0 font-normal">
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={user.avatar} alt={user.name} />
-                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                    <AvatarImage
+                      src={user.avatar}
+                      alt={user.name.slice(0, 2)}
+                    />
+                    <AvatarFallback className="rounded-lg">
+                      {user.name.slice(0, 2)}
+                    </AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
                     <span className="truncate font-medium">{user.name}</span>
@@ -81,28 +88,28 @@ export function NavUser({
               </DropdownMenuLabel>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
+            {/* <DropdownMenuGroup> */}
+            {/*   <DropdownMenuItem> */}
+            {/*     <Sparkles /> */}
+            {/*     Upgrade to Pro */}
+            {/*   </DropdownMenuItem> */}
+            {/* </DropdownMenuGroup> */}
+            {/* <DropdownMenuSeparator /> */}
+            {/* <DropdownMenuGroup> */}
+            {/*   <DropdownMenuItem> */}
+            {/*     <BadgeCheck /> */}
+            {/*     Account */}
+            {/*   </DropdownMenuItem> */}
+            {/*   <DropdownMenuItem> */}
+            {/*     <CreditCard /> */}
+            {/*     Billing */}
+            {/*   </DropdownMenuItem> */}
+            {/*   <DropdownMenuItem> */}
+            {/*     <Bell /> */}
+            {/*     Notifications */}
+            {/*   </DropdownMenuItem> */}
+            {/* </DropdownMenuGroup> */}
+            {/* <DropdownMenuSeparator /> */}
             <DropdownMenuItem
               onClick={() => {
                 logout()
