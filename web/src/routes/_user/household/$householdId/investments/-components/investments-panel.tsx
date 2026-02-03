06a73f0cb13a3eab165d2fc58d4a1fcd0799f6ee
Joey Yu <joey@jyu.dev>
2026-02-02 22:20:45 -0500

    remove relative from accordion trigger
    Signed-off-by: Joey Yu <joey@jyu.dev>

--- web/src/routes/_user/household/$householdId/investments/-components/investments-panel.tsx
+++ web/src/routes/_user/household/$householdId/investments/-components/investments-panel.tsx
@@ -278,7 +278,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          '**:data-[slot=accordion-trigger-icon]:text-muted-foreground group/accordion-trigger relative flex flex-1 items-start justify-between gap-6 border border-transparent p-2 text-left text-xs/relaxed font-medium transition-all outline-none hover:underline disabled:pointer-events-none disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4',
+          '**:data-[slot=accordion-trigger-icon]:text-muted-foreground group/accordion-trigger flex flex-1 items-start justify-between gap-6 border border-transparent p-2 text-left text-xs/relaxed font-medium transition-all outline-none hover:underline disabled:pointer-events-none disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4',
           className,
         )}
         {...props}
