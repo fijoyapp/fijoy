6c663372c9a8dbc57573e8a5c5b83a4b993c951d
Joey Yu <joey@jyu.dev>
2026-02-04 23:22:48 -0500

    add down
    Signed-off-by: Joey Yu <joey@jyu.dev>

---
+++ ent/migrate/migrations/20260205041036_transaction-cascade.down.sql
@@ -0,0 +1,4 @@
+-- reverse: modify "transaction_entries" table
+ALTER TABLE "transaction_entries" DROP CONSTRAINT "transaction_entries_transactions_transaction_entries", ADD CONSTRAINT "transaction_entries_transactions_transaction_entries" FOREIGN KEY ("transaction_id") REFERENCES "transactions" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
+-- reverse: modify "investment_lots" table
+ALTER TABLE "investment_lots" DROP CONSTRAINT "investment_lots_transactions_investment_lots", ADD CONSTRAINT "investment_lots_transactions_investment_lots" FOREIGN KEY ("transaction_id") REFERENCES "transactions" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
